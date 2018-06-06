d2a2a4b7998f70cd30d6a6c764af3bf8b77ba6e4
zhu-ty <zhu-ty@outlook.com>
2018-06-05 21:52:46 -0700

    modify the CMakeLists.txt to make it run in Ubuntu 16.04

--- Common/SysUtil.hpp
+++ Common/SysUtil.hpp
@@ -14,8 +14,11 @@
 
 #ifdef WIN32
 #include <Windows.h>
-#endif
 #include <direct.h>
+#endif
+
+#include <stdarg.h>
+
 #include <chrono>
 #include <memory>
 #include <thread>
@@ -31,17 +34,18 @@ enum class ConsoleColor {
 };
 
 #ifndef WIN32
-#define BLACK_TEXT(x) "\033[30;1m" x "\033[0m"
-#define RED_TEXT(x) "\033[31;1m" x "\033[0m"
-#define GREEN_TEXT(x) "\033[32;1m" x "\033[0m"
-#define YELLOW_TEXT(x) "\033[33;1m" x "\033[0m"
-#define BLUE_TEXT(x) "\033[34;1m" x "\033[0m"
-#define MAGENTA_TEXT(x) "\033[35;1m" x "\033[0m"
-#define CYAN_TEXT(x) "\033[36;1m" x "\033[0m"
-#define WHITE_TEXT(x) "\033[37;1m" x "\033[0m"
+#define BLACK_TEXT(x) "\033[30;1m" << x << "\033[0m"
+#define RED_TEXT(x) "\033[31;1m" << x << "\033[0m"
+#define GREEN_TEXT(x) "\033[32;1m" << x << "\033[0m"
+#define YELLOW_TEXT(x) "\033[33;1m" << x << "\033[0m"
+#define BLUE_TEXT(x) "\033[34;1m" << x << "\033[0m"
+#define MAGENTA_TEXT(x) "\033[35;1m" << x << "\033[0m"
+#define CYAN_TEXT(x) "\033[36;1m" << x << "\033[0m"
+#define WHITE_TEXT(x) "\033[37;1m" << x << "\033[0m"
 #endif
 
 class SysUtil {
+	#define COMMAND_STRING_LENGTH 256
 public:
 	/***********************************************************/
 	/*                    mkdir function                       */
